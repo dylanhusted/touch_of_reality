1b718e27dbdb02b4ca5f41ee2cde0b3fe1469871
Dylan Husted <dhusted1@babson.edu>
2017-05-09 19:43:14 -0400

    Connect to Pushbullet stream, log messages in Unity console

---
+++ Assets/Scripts/httpTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.IO;
+
+public class httpTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(GetNotifications());
+    }
+
+    IEnumerator GetNotifications()
+    {
+        // Get TextMesh
+        TextMesh pointsText = GameObject.Find("apiResponse").GetComponent<TextMesh>();
+        pointsText.text = "Ready.";
+        // Call API
+        WWWForm form = new WWWForm();
+        var headers = new Dictionary<string, string>();
+        headers.Add("Access-Token", "o.czs8GXB8DKYttsEtWsBb991gQdHSsGSE");
+        WWW www = new WWW("https://api.pushbullet.com/v2/pushes", null, headers);
+        yield return www;
+        Debug.Log("2. " + www.text);
+        // Parse JSON
+        Response notif = JsonUtility.FromJson<Response>(www.text);
+        // Iterate over each Push
+        for (int i = 0; i < notif.pushes.Count; i++)
+        {
+            // Display notification, based on push type
+            if (notif.pushes[i].type == "note")
+            {
+                Debug.Log(notif.pushes[i].sender_name + ": " + notif.pushes[i].body);
+            }
+            else if (notif.pushes[i].type == "file")
+            {
+                Debug.Log(notif.pushes[i].sender_name + " sent you a file");
+            }
+            else
+            {
+                Debug.Log(notif.pushes[i].sender_name + ": [TORE-error] Unknown notification type");
+            }
+        }
+        // Set TextMesh
+        pointsText.text = www.text;
+    }
+
+    [System.Serializable]
+    public class Response
+    {
+        public List<Push> pushes;
+    }
+
+    [System.Serializable]
+    public class Push
+    {
+        public string type;
+        public string sender_name;
+        public string body;
+    }
+}
---
+++ Assets/Scripts/wsTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using SimpleJSON;
+
+public class wsTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(StreamNotifications());
+    }
+
+    IEnumerator StreamNotifications()
+    {
+        WebSocket w = new WebSocket(new Uri("wss://stream.pushbullet.com/websocket/o.czs8GXB8DKYttsEtWsBb991gQdHSsGSE"));
+        yield return StartCoroutine(w.Connect());
+        while (true)
+        {
+            string reply = w.RecvString();
+            if (reply != null)
+            {
+                // Debug.Log("Received: " + reply);
+                var resp = JSON.Parse(reply);
+                if (resp["type"] == "push")
+                {
+                    if (resp["push"]["type"] == "sms_changed")
+                    {
+                        Debug.Log(resp["push"]["notification"]["title"] + ": " + resp["push"]["notification"]["body"]);
+                    }
+                    //if (resp["push"]["type"] == "mirror")
+                    //{
+                    //    if (resp["push"]["notification"]["application_name"] == "Messenger")
+                    //    {
+                    //        Debug.Log(resp["push"]["notification"]["title"] + ": " + resp["push"]["notification"]["body"]);
+                    //    }
+                    //}
+                }
+            }
+            if (w.error != null)
+            {
+                Debug.LogError("Error: " + w.error);
+                break;
+            }
+            yield return 0;
+        }
+        w.Close();
+    }
+}
